385228ba520b8c4e364339d4b708a7a7c17f358e
Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>
2025-05-22 19:04:57 +0530

    Fixed the responsiveness first Screen
    Signed-off-by: Anil_Kumar_Majji <106086523+Anil-kumar-Majji@users.noreply.github.com>

--- mock-relying-party-ui/src/travelPass/pages/auth/LoginForm.js
+++ mock-relying-party-ui/src/travelPass/pages/auth/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
 function LoginForm({ sentOtp }) {
@@ -14,6 +14,53 @@ function LoginForm({ sentOtp }) {
     { icon: 'images/access_anywhere_icon.png', name: 'Access Anytime, Anywhere' },
     { icon: 'images/arrows_right_icon.png', name: 'Smooth Border Entry' }
   ]
+
+  const Carousel = ({ menu }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+      const timer = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % menu.length);
+      }, 3000);
+
+      return () => clearInterval(timer);
+    }, [menu.length, 3000]);
+
+    const nextSlide = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % menu.length);
+    };
+
+    const prevSlide = () => {
+      setCurrentIndex(
+        (prevIndex) => (prevIndex - 1 + menu.length) % menu.length
+      );
+    };
+
+
+    return (
+      <div className="relative w-full max-w-lg mx-auto">
+        <div className="overflow-hidden">
+          <div className="flex transition-transform duration-500" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+            {menu.map((opt, index) => (
+              <div key={index} className="flex-shrink-0 w-full space-y-3">
+                <img src={opt.icon} alt={`${opt.name}`} className="h-16 w-16 place-self-center" />
+                <p class="text-normal font-[500] text-[#8972b9] place-self-center">{opt.name}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div class="flex justify-between items-center">
+          <button onClick={prevSlide} className="h-20 font-[700] text-[#7F56D9] p-2 rounded-full"> ❮ </button>
+          <div class={`h-3 w-3 rounded-lg ${currentIndex!==0 ? 'bg-[#c0b0e2]' : 'bg-[#8d5ff0]'}`}></div>
+          <div class={`h-3 w-3 rounded-lg ${currentIndex!==1 ? 'bg-[#c0b0e2]' : 'bg-[#8d5ff0]'}`}></div>
+          <div class={`h-3 w-3 rounded-lg ${currentIndex!==2 ? 'bg-[#c0b0e2]' : 'bg-[#8d5ff0]'}`}></div>
+          <div class={`h-3 w-3 rounded-lg ${currentIndex!==3 ? 'bg-[#c0b0e2]' : 'bg-[#8d5ff0]'}`}></div>
+          <button onClick={nextSlide} className="h-20 font-[700] text-[#7F56D9] p-2 rounded-full"> ❯ </button>
+        </div>
+      </div>
+    );
+  };
+
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -44,13 +91,13 @@ function LoginForm({ sentOtp }) {
 
   return (
     <div className="flex flex-col space-y-3">
-      <div className="flex gap-x-6 mb-[0.2rem] md:justify-between">
-        <img src='images/truck_pass_welcome_page.png' alt='welcomeImage' className="w-[58%] md:h-[380px] xl:h-[450px] 2xl:h-[750px]" />
+      <div className="md:flex gap-x-6 mb-[0.2rem] md:justify-between">
+        <img src='images/truck_pass_welcome_page.png' alt='welcomeImage' className="h-[220px] md:h-[350px] lg:h-[460px] xl:h-[560px] w-full md:w-[66%]" />
 
         <div className="bg-white flex items-center w-[30%]">
-          <div className="w-full max-w-sm md:space-y-8">
+          <div className="w-full max-w-sm space-y-2 md:space-y-4 py-5 px-3 md:p-0">
             <h2 className="md:text-lg xl:text-2xl 2xl:text-3xl font-bold mb-2 text-[#101828]">Log In</h2>
-            <form className="space-y-6 w-full">
+            <form className="space-y-4 w-full">
               <div>
                 <input
                   value={inputValue}
@@ -58,19 +105,19 @@ function LoginForm({ sentOtp }) {
                   onKeyDown={handleKeyDown}
                   type="text"
                   placeholder="Enter Your Email ID"
-                  className="md:text-xs lg:text-sm sm:w-[70%] xl:w-[80%] px-4 py-1.5 border border-[#D0D5DD] rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 font-semibold"
+                  className="md:text-xs lg:text-sm sm:w-[70%] xl:w-[84%] px-4 py-1.5 border border-[#D0D5DD] rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 font-semibold"
                 />
                 {idError && <div className="text-red-700 text-xs lg:text-sm">{idError}</div>}
               </div>
               <button type="button" onClick={handleLogin}
-                className={`${!inputValue ? 'bg-[#b1b0b2] cursor-default' : 'bg-purple-600 hover:bg-[#7F56D9] cursor-pointer'} md:text-xs lg:text-sm sm:w-[70%] xl:w-[80%] font-semibold py-1.5 text-white rounded-md hover: transition bg-opacity-100`}>
+                className={`${!inputValue ? 'bg-[#b1b0b2] cursor-default' : 'bg-purple-600 hover:bg-[#7F56D9] cursor-pointer'} text-xs lg:text-sm w-[120px] md:w-[81%] font-semibold py-1.5 text-white rounded-sm md:rounded-md hover:transition bg-opacity-100`}>
                 Log in with OTP
               </button>
             </form>
           </div>
         </div>
       </div >
-      <div className=" flex justify-evenly py-6 lg:h-[7rem] 2xl:h-[8rem]">
+      <div className="hidden md:flex justify-evenly py-6 lg:h-[7rem] 2xl:h-[8rem]">
         {footerMenu.map((item) => {
           return (
             <div className="flex-col space-y-[4%] sm:text-xs lg:text-sm 2xl:text-xl">
@@ -79,6 +126,9 @@ function LoginForm({ sentOtp }) {
             </div>
           )
         })}
+      </div>
+      <div className="p-4 md:hidden">
+        <Carousel menu={footerMenu} />
       </div>
     </div>
   );
--- mock-relying-party-ui/src/travelPass/shared/Navbar.js
+++ mock-relying-party-ui/src/travelPass/shared/Navbar.js
@@ -11,6 +11,7 @@ const languages = {
 
 const Navbar = () => {
     const [isDropdownOpen, setDropdownOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const { i18n, t } = useTranslation();
     const [selectedLanguage, setSelectedLanguage] = useState(languages[i18n.language]);
     const langDropdownRef = useRef(null);
@@ -36,35 +37,45 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="w-full bg-[#FFFFFF] px-[4%] py-[1.2%] text-black flex items-center justify-between">
-            <img src='images/truck_pass_apply_title.png' className='w-[27%]' />
+        <nav className="w-full bg-[#FFFFFF] p-[4%] px-[3%] md:py-[1.2%] text-black flex items-center justify-between">
+            <div className={`absolute flex-col md:hidden  ${isOpen ? 'w-36 p-[5%] left-0 bg-[#FFFFFF] top-0' : 'w-4 top-0'} duration-500`}>
+                <div className={`space-y-1 cursor-pointer ${isOpen ? 'ml-[84%]' : 'ml-[4%] mt-3.5'} duration-700`} onClick={() => setIsOpen(!isOpen)}>
+                    <div className='w-[17px] border-[#000000] border'></div>
+                    <div className='w-[17px] border-[#000000] border'></div>
+                    <div className='w-[17px] border-[#000000] border'></div>
+                </div>
+                <div className={`${isOpen ? 'flex-col gap-y-4 h-[300px] mt-4' : 'hidden'}`}>
+                    <p>Home</p>
+                    <p>Help</p>
+                </div>
+            </div>
+
+            <img src='images/truck_pass_apply_title.png' className='w-[150px] ml-7 md:ml-0 md:w-[200px] lg:w-[275px] xl:w-[380px]' />
             <div className="flex space-x-6 text-[#202020] ">
-                <div onClick={handleNavOptions} className="flex space-x-6 items-center text-sm md:text-normal 2xl:text-2xl">
-                    <button id='home' className='cursor-pointer font-semibold'>Home</button>
-                    <button id='help' className='cursor-pointer font-semibold'>Help</button>
+                <div onClick={handleNavOptions} className="flex space-x-2 md:space-x-4 lg:space-x-6 xl:space-x-9 items-center text-[10px] sm:text-sm md:text-normal lg:text-lg xl:text-xl">
+                    <button id='home' className='hidden md:flex cursor-pointer font-semibold'>Home</button>
+                    <button id='help' className='hidden md:flex cursor-pointer font-semibold'>Help</button>
                     <div className="relative inline-block text-left" ref={langDropdownRef}>
                         <button
                             onClick={() => setDropdownOpen(!isDropdownOpen)}
-                            className="flex items-center rounded-lg font-semibold cursor-pointer text-sm md:text-normal 2xl:text-2xl"
+                            className="flex items-center rounded-lg font-semibold cursor-pointer sm:text-sm md:text-normal lg:text-lg 2xl:text-2xl"
                         >
-                            <img src='images/globe_icon.png' alt='dropdown_icon' className='h-[1rem] md:h-4 2xl:h-6 px-1' />
-                            {selectedLanguage.label}
-                            <img src='images/dropdown_icon.png' alt='dropdown_icon' className='md:1.5rem lg:h-auto  lg:pt-1 px-1'/>
+                            <img src='images/globe_icon.png' alt='dropdown_icon' className='h-[12px] md:h-[16px] lg:h-[18px] xl:h-[20px] px-1' />
+                            <p class="text-[10px] sm:text-sm md:text-normal lg:text-lg xl:text-xl">{selectedLanguage.label}</p>
+                            <img src='images/dropdown_icon.png' alt='dropdown_icon' className='h-[5px] md:h-[7px] lg:h-auto lg:pt-1 px-1' />
                         </button>
 
                         {isDropdownOpen && (
-                            <div className="flex absolute mt-2 w-28 bg-white rounded-lg shadow-lg z-30">
-                                <ul className="py-1 duration-200" onClick={handleLanguageChange}>
-                                    <li id='en' className="px-4 py-2 hover:bg-gray-100 cursor-pointer">English</li>
-                                    <li id='fr' className="px-4 py-2 hover:bg-gray-100 cursor-pointer">French</li>
-                                    <li id='ar' className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Arabic</li>
+                            <div className="flex absolute mt-[1%] w-[66px] md:w-[90px] lg:w-[108px] bg-white rounded-b-lg shadow-lg z-30 border-r border-l duration-500">
+                                <ul className="py-1 duration-200 w-full" onClick={handleLanguageChange}>
+                                    <li id='en' className="px-2 py-2 hover:bg-gray-100 cursor-pointer">English</li>
+                                    <li id='fr' className="px-2 py-2 hover:bg-gray-100 cursor-pointer">French</li>
+                                    <li id='ar' className="px-2 py-2 hover:bg-gray-100 cursor-pointer">Arabic</li>
                                 </ul>
                             </div>
                         )}
                     </div>
                 </div>
-
-
                 {/* <NavbarItem label="Home" to="/" />
         <NavbarItem label="About" to="/about" />
         <NavbarItem label="Contact" to="/contact" /> */}
